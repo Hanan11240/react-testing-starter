3a065d855786c05d2cee272439f24254bb6fd3c7
hanan.naseer <hanan.naseer@insuriq.com>
2026-07-15 14:21:47 +0530

    add test ui

---
+++ src/tests/components/Greet.test.tsx
@@ -0,0 +1,23 @@
+import {render,screen } from  "@testing-library/react"
+import Greet from '../../components/Greet'
+
+
+describe("Greet",()=>{
+    it("should render hello with the name when name is provided",()=>{
+        render(<Greet name="John" />)
+
+
+       const heading =  screen.getByRole("heading");
+       expect(heading).toBeInTheDocument();
+       expect(heading).toHaveTextContent(/hello John/i)
+        
+    })
+
+    it("should render login button when name is not provided",()=>{
+        render(<Greet name="" />)
+        const button  = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveTextContent(/login/i)
+    })
+})
+
